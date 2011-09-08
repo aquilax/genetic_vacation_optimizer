02e7e7cec983e140bb4f4830ff3caabc440fdc03
Evgeni Vasilev <aquilax@gmail.com>
2011-09-08 16:02:49 +0300

    Sex is very stupid, should be improved

--- vacation.js
+++ vacation.js
@@ -8,11 +8,12 @@ var Vacation = (function(){
     yearDays: 365,
     vacationDays: 20,
     firstDay: 0,
-    individualsInGen: 20,
-    generations: 10,
+    individualsInGen: 60,
+    generations: 40,
     random_select: 0.05,
     retain: 0.2,
-    mutate: 0.01
+    mutate: 0.1,
+    mutation_strength: 0.8
   };
   
   //Day of year to day of week
@@ -48,7 +49,8 @@ var Vacation = (function(){
   function fitness(individual){
     var groups = {};
     var ing = 0;
-    for (i in individual) {
+    var l = individual.length;
+    for (var i = 0; i < l; i++) {
       if (individual[i]){
         ing++;
       } else {
@@ -70,8 +72,10 @@ var Vacation = (function(){
       }
     }
     var result = 0;
-    for (k in groups){
-      result += Math.pow(groups[k], k);
+    for (k in groups) {
+      if (k > 2) {
+        result += Math.pow(groups[k], k);
+      }
     }
     return result;
   }
@@ -90,29 +94,67 @@ var Vacation = (function(){
         days--;
       }
     }
-    return g;
+    return {v: g, grade: fitness(g)};
   }
     
   function population(individuals) {
     var population = [];
     for (var i = 0; i < individuals; i++){
-      var ind = individual()
-      population[i] = {
-        v: ind,
-        grade: fitness(ind)
-      };
+      population[i] = individual();
     }
     return population;
   }
   
   function mutate(individual){
-    //TODO: Mutate
-    return individual;
+    var mutant = {v: []};
+    var mutations = 0;
+    var l = individual.v.length;
+    for (var d = 0; d < l; d++){
+      mutant.v[d] = individual.v[d];
+      if (mutant.v[d] == 3 && conf.mutation_strength > Math.random()){
+        mutant.v[d] = 0;
+        mutations++;
+      }
+    }
+    while (mutations > 0) {
+      ndx = rand(0, conf.yearDays);
+      if (mutant.v[ndx] == 0){
+        mutant.v[ndx] = 3;
+        mutations--;
+      }
+    }
+    mutant.grade = fitness(mutant.v);
+    return mutant;
   }
   
   function sex(father, mother){
-    //TODO: Do the sexy thing
-    return mother;
+    var child = {v: []};
+    var extras = 0;
+    var ona = [];
+    var l = father.v.length;
+    for (var d = 0; d < l; d++){
+      child.v[d] = mother.v[d];
+      if (father.v[d] == 3) {
+        if (mother.v[d] != 3){
+          child.v[d] = 3;
+          extras++;
+        } else if (child.v[d+1] != 3) {
+          child.v[d+1] = 3;
+        }
+      }
+      if (child.v[d] == 3){
+        ona.push(d);
+      }
+    }
+    while (extras > 0){
+      var ndx = rand(0, ona.length-1);
+      var el = ona.splice(ndx, 1);
+      el = parseInt(el[0]);
+      child.v[el] = 0;
+      extras--;
+    }
+    child.grade = fitness(child.v);
+    return child;
   }
   
   function evolve(generation) {
@@ -126,36 +168,44 @@ var Vacation = (function(){
         parents.push(generation[i]);
       }
     }
-    for (var i in parents){
+    l = parents.length;
+    for (var i = 0; i < l; i++){
       if (conf.mutate > Math.random()){
-        var mutant = mutate(parents[i]);
-        parents[i] = {
-          v: mutant,
-          grade: fitness(mutant)
-        };
+        parents[i] = mutate(parents[i]);
       }
     }
+    var pl = parents.length;
     while (parents.length < conf.individualsInGen){
-      male = rand(0, parents.length);
-      female = rand(0, parents.length);
+      var male = rand(0, pl);
+      var female = rand(0, pl);
       if (male != female){
         var child = sex(parents[male], parents[female]);
-        parents.push({
-          v: child,
-          grade: fitness(child)
-        });
+        parents.push(child);
       }
     }
     return parents;
   }
   
-  function print(vals){
+  function count3(individual){
+    var cnt = 0;
+    var l = individual.v.length;
+    for(var i = 0; i < l; i++){
+      if (individual.v[i] == 3){
+        cnt++;
+      }
+    }
+    return cnt;
+  }
+  
+  function print(vals, caption){
     offset = conf.firstDay == 0?6:conf.firstDay-1;
-    var t = '<table><tr>';
+    var t = '<table><tr><caption>'+caption+'</caption>';
+    
     for (i = 0; i < offset; i++){
       t += '<td>&nbsp;</td>';
     }
-    for (i in vals){
+    l = vals.length;
+    for (i  = 0; i < l; i++){
       switch(vals[i]){
         case 1: t += '<td style="background-color:#f00">'+i+'</td>'; break;
         case 2: t += '<td style="background-color:#0f0">'+i+'</td>'; break;
@@ -167,21 +217,20 @@ var Vacation = (function(){
       }
     }
     t += "</tr></table>";
-    document.write(t);
+    $('#cont').append("<td>"+t+"</td>");
   }
   
   function run(){
     generation = population(conf.individualsInGen);
     for (var i = 0; i < conf.generations; i++){
       generation.sort(function (a, b){return b.grade - a.grade;});
-      document.write("<b>"+i+": "+generation[0].grade+"</b><br />");
+      print(generation[0].v, i+":"+generation[0].grade);
       generation = evolve(generation);
     }
     generation.sort(function (a, b){return b.grade - a.grade;});
-    document.write("<b>"+i+": "+generation[0].grade+"</b><br />");
-    print(generation[0].v);
+    print(generation[0].v, i+":"+generation[0].grade);
     //document.write(generation[0].v);
-    console.log(generation[0].grade);
+    //console.log(generation[0].grade);
   }
   
   return {
